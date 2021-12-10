91368ceb2a0d6f747cc579035c7b25fe464cd1f4
satyam <satyamrocker1234@gmail.com>
2021-12-10 17:22:53 +0530

    added db connect

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
-
+const connectDB = require("./config/db.js");
+connectDB();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`server running in ${process.env.NODE_ENV} on ${PORT}`);
+app.use("/", (req, res) => {
+  res.send("api is running");
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} on ${PORT}`)
+);
